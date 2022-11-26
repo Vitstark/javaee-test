5df08e9c89236034de1048ba8cc8acb0173ddd8f
Vitaly Chekushkin <v.chekushkin@micord.ru>
2022-11-27 01:20:04 +0300

    resource changed

--- src/main/java/com/example/javaeetestdocker/DateServlet.java
+++ src/main/java/com/example/javaeetestdocker/DateServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDate;
 
-@WebServlet(name = "DateServlet", value = "/DateServlet")
+@WebServlet("/date")
 public class DateServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
